d1948ce6601cef6a84e75b461721eb04337378d9
loecmanev <82966887+loecmanev@users.noreply.github.com>
2025-06-04 14:36:10 +0700

    Update Euler Method.py

--- Euler Method.py
+++ Euler Method.py
@@ -24,6 +24,6 @@ for i in range(iteration):
     ti[i+1] = t+h
     yi[i+1] = yiplus1
     
-print(f"Jadi nilai y ketika t = 100 adalah {yi[-1]}")
+print(f"Jadi nilai y ketika t = 100 adalah {yi[-1]} atau {yi[-1]:.5e}")
 
 
